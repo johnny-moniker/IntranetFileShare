b9051dbbdb8eb284031adc3527140b849a5e64bc
Jefairfi@gmail.com <Arcti311>
2016-02-01 20:57:25 -0700

    Added upload file ui stuff.

---
+++ MVC5/Controllers/FileUploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            return Json(new { Message = file });
+            //TODO Handle the uploaded file here
+        }
+    }
+}
